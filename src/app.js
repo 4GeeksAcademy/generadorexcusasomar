6fd3a7a6dff0ddea138c4dac04fc7f2fbd8ba5d0
Omar Páez <135053433+omarpaezdev@users.noreply.github.com>
2025-06-14 11:29:30 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
-import "bootstrap";
-import "./style.css";
+let d = document;
+function generateExcuse(){ 
+    let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+    let action = ['ate', 'peed', 'crushed', 'broke'];
+    let what = ['my homework', 'my phone', 'the car'];
+    let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
 
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+    let numWho =  Math.floor((Math.random() * who.length) ) 
+    let numAction =  Math.floor((Math.random() * action.length)  )
+    let numWhat = Math.floor((Math.random() * what.length) )
+    let numWhen = Math.floor((Math.random() * when.length) )
+    d.getElementById("excuse").innerHTML = `${who[numWho]} ${action[numAction]} ${what[numWhat]} ${when[numWhen]} ` 
+    
+}
+d.onload = generateExcuse();
